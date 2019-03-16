9a9d7ab031a4d6d48742e350f9a097a0375e09c0
team awesome <maxj@email.arizona.edu>
2019-03-16 13:39:53 -0700

    submit button and firebase

---
+++ app.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyCofRVed4C8xXIJYm9s4p_qTNFUu8UR1PA",
+    authDomain: "number-1-team.firebaseapp.com",
+    databaseURL: "https://number-1-team.firebaseio.com",
+    projectId: "number-1-team",
+    storageBucket: "",
+    messagingSenderId: "1024556041499"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
+  $(document).ready(function (){
+    console.log(URL);
+  });
