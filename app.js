37c3fb7283c21f76082db5223823a009817a3e42
team awesome <maxj@email.arizona.edu>
2019-03-16 14:32:37 -0700

    update to js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var config = {
-    apiKey: "AIzaSyCofRVed4C8xXIJYm9s4p_qTNFUu8UR1PA",
-    authDomain: "number-1-team.firebaseapp.com",
-    databaseURL: "https://number-1-team.firebaseio.com",
-    projectId: "number-1-team",
-    storageBucket: "",
-    messagingSenderId: "1024556041499"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCofRVed4C8xXIJYm9s4p_qTNFUu8UR1PA",
+  authDomain: "number-1-team.firebaseapp.com",
+  databaseURL: "https://number-1-team.firebaseio.com",
+  projectId: "number-1-team",
+  storageBucket: "",
+  messagingSenderId: "1024556041499"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+
+$(document).ready(function () {
+  //console.log(config);
+
   
-  $(document).ready(function (){
-    console.log(URL);
-  });
+});
