db4348fa048c9d0ac9a8b9108ccc69399fe9ea94
FizzyMo <carisasaenz@gmail.com>
2019-03-19 18:13:16 -0700

    finished website just needs to polish

--- javascript/app.js
+++ javascript/app.js
@@ -6,99 +6,59 @@ var config = {
   storageBucket: "",
   messagingSenderId: "1024556041499"
 };
+  
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
 $(document).ready(function () {
   //console.log(config);
   $('#add-article').on('click', function (event) {
 
-    console.log("add article clicked'")
-
+    //console.log("add article clicked");
     event.preventDefault;
-
-    // var linkName = $("#link").val().trim();
-
-    // var sourceName = $("#source").val().trim();
-
-    // var toneName = $("#tone").val().trim();
-
-    // var articleName = $("#article").val().trim();
-
-    // database.ref().push({
-
-    //   url: "https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=496e966f5c324e3080abd07b9111c5c3"
-
-    // })
-
     var url = "https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=496e966f5c324e3080abd07b9111c5c3"
-
-    // var req = new Request(url);
-
-    // fetch(req)
-
-    //   .then(function (response) {
-    //   })
-
-    var queryURL = "https://api-watson.herokuapp.com/api/watson/"
      
     $.ajax({
-
       url: url,
-
       method: "GET"
     })
 
+
       .then(function (response) {
-
-        console.log(response)
-
-        // var newsDiv = $("<div class=''>");
-
-        // var description = response.articles[0].description;
-
-        // var content = $("<p>").text("description" + description);
-        // var url = response.articles[0].url;
-        // newsDiv.append(content);
-
+        //console.log(response)
         for (var i = 0; i <= response.articles.length; i++) {
-
           var url = response.articles[i].url;
           var description = response.articles[i].description;
-
-          var data = {
-            "url": url,
-            "features": {
-              "emotion": {
-                "document": [
-                  true
-                ]
-              }
-            }
-          }
-    
-          $.ajax(queryURL,
-            {
-              type: "POST",
-              data: data
-            }).done(function (data) {
-              console.log(data);
-              var tone =  (Object.keys(data.emotion.document.emotion).reduce(function (a, b) { return data.emotion.document.emotion[a] > data.emotion.document.emotion[b] ? a : b }));
-        
-          var row = $("<tr>");
-          var cols = '<td id="source">' + description + '</td>' +
-            '<td id="link">' + "<a href=" + url + ">" + url+ "</a>" + '</td>' +
-            '<td id="tone">' + tone + '</td>';
-
-          row.append(cols);
-          $("#info tbody").append(row)
-            })
+          var content = response.articles[i]["content"];
+          displayData(response.articles[i].url, response.articles[i]["content"])
         }
-
-
       })
 
   });
 
 });
+
+function displayData(url, content){
+          var queryURL = "https://api-watson.herokuapp.com/api/watson/"         
+          
+          $.ajax(queryURL,
+            {
+              type: "POST",
+              data: {
+                        "url": url,
+                        "features": {
+                              "emotion": {
+                              "document": [true]
+                              }
+                        }
+              }
+            }).done(function (response) {
+            var tone = (Object.keys(response.emotion.document.emotion).reduce(function (a, b) { return response.emotion.document.emotion[a] > response.emotion.document.emotion[b] ? a : b }));
+            var row = $("<tr>");
+            var cols = '<td id="link"><a style="text-decoration: none" href="' + url + '" target = "_blank" > ' + content + '</a></td>' +
+                       '<td id="tone">' + tone + '</td>';
+
+              row.append(cols);
+              $("#info tbody").append(row)
+            })
+}
